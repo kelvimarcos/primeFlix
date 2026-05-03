21b7a06453b2f48806b37ebb870a54995b46508f
Kelvison Marcos <kelvison.marcos10@gmail.com>
2026-05-03 00:19:15 -0300

    atualização routes

--- src/routes.js
+++ src/routes.js
@@ -2,30 +2,27 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Filme from "./pages/Filme/Filme";
-
 import Favoritos from "./pages/Favoritos";
 
-import Erro from './pages/Erro/';
+import Erro from "./pages/Erro/";
 
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-
 function RoutesApp() {
-    return (
-        <BrowserRouter>
-            <Header/>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/filme/:id" element={<Filme />} />
-                <Route path="/favoritos" element={<Favoritos />} />
+  return (
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
 
-                <Route path="*" element={<Erro />} />
-            </Routes>
-            <Footer/>
-        </BrowserRouter>
-    );
-
+        <Route path="*" element={<Erro />} />
+      </Routes>
+      <Footer />
+    </HashRouter>
+  );
 }
 
 export default RoutesApp;
