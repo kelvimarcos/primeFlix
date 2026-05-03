883912e102679c77f7c709f46edcbaaf81ff80b2
Kelvison Marcos <kelvison.marcos10@gmail.com>
2026-05-03 00:21:32 -0300

    atualizando routes

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import Footer from "./components/footer";
 
 function RoutesApp() {
   return (
-    <HashRouter>
+    <HashRouter basename="/primeFlix">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
