cf9d8ce3961b57d42da25b5790345639eb59c6e9
Kelvison Marcos <kelvison.marcos10@gmail.com>
2026-05-05 20:26:47 -0300

    config deploy

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import Footer from "./components/footer";
 
 function RoutesApp() {
   return (
-    <HashRouter basename="/primeFlix">
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
